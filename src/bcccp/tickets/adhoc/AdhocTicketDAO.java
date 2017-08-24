b98dd8cf8bdba66195e0cf2f463180d430c0897f
mjaved06 <mjaved.jazz@gmail.com>
2017-08-25 02:16:13 +1000

    Added code to AdhocTicketDAO.java
    Added code and comment .ready to review .

--- src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -4,20 +4,31 @@ import java.util.List;
 
 public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 
+    //Defining class types and variables
+	
 	private IAdhocTicketFactory factory;
 	private int currentTicketNo;
 
-	
+	//Declaration of the list variable 
+    
+	private List<IAdhocTicket> issued_Adhoc_Tickets; 
+
 	
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
-		//TODO Implement constructor
+		this.issued_Adhoc_Tickets = new ArrayList<IAdhocTicket>(); 
+ 		this.factory = factory; 
+                this.currentTicketNo = 1; 
+
 	}
 
 
-
+    //This overrided method will create a ticket and return it
 	@Override
 	public IAdhocTicket createTicket(String carparkId) {
-		// TODO Auto-generated method stub
+		IAdhocTicket ticket = factory.make(carparkId, currentTicketNo++); 
+ 
+     		list.add(ticket); 
+
 		return null;
 	}
 
