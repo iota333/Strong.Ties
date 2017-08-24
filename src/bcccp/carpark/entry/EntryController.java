6e91e680d732219ffb4ecf6fe69b934e403e18db
ashaik10 <30788158+ashaik10@users.noreply.github.com>
2017-08-25 01:33:16 +1000

    Added code for methods to entrycontroller
    Remaining methods

--- src/bcccp/carpark/entry/EntryController.java
+++ src/bcccp/carpark/entry/EntryController.java
@@ -79,26 +79,82 @@ if (state.equals(STATE.WAITING)) {
 
 	@Override
 	public void ticketTaken() {
-		// TODO Auto-generated method stub
-		
+	if (state.equals(STATE.ISSUED) || state.equals(STATE.VALIDATED)) {
+ 			setState(STATE.TAKEN);
+ 		} else {
+ 			ui.beep();
+ 		}
+  	}			
 	}
 
 
 
 	@Override
 	public void notifyCarparkEvent() {
-		// TODO Auto-generated method stub
-		
+			if (state.equals(STATE.ISSUED) || state.equals(STATE.VALIDATED)) {
+ 		setState(STATE.TAKEN);
+ 		} else {
+ 			ui.beep();
+ 		}
+  	}	
 	}
+private void log(String message) {
+ 		System.out.println("Exit Controller : " + message);
+ 	}
+   	private void setState(STATE newState) {
+ 		switch (newState) {
+ 		case BLOCKED:
+ 			log("set State : BLOCKED");
+ 			state = STATE.BLOCKED;
+ 		break;
+			ui.display("BLOCKED");
+ 
+ 		case IDLE:
+ 			log("set State : IDLE");
+ 			state = STATE.IDLE;
+			ui.display("IDLE");
+			break;
+ 		default:
+ 			break;
+ 			// In-progress to do the other status
+ 			// Need to do EXITED, REJECTED, WAITING, EXITING, PROCESSED, TAKEN, ISSUED, VALIDATED, FULL
+ 			// status as like above
+ 		}
+ 	}
+ 	// This method used to do the car park event
+  	@Override
+  	public void carEventDetected(String detectorId, boolean detected) {
+ 		// TODO Auto-generated method stub
+ 		
+ 		log("Car Event Detected: " + detectorId + "Car Detected" + detected);
+ 		switch (state) {
+ 		case BLOCKED:
+ 			if (detectorId.equals(insideSensor.getId()) && !detected) {
+ 				setState(initState);
+ 			}
+ 			break;
+ 		case IDLE:
+ 			if (detectorId.equals(insideSensor.getId()) && detected) {
+ 				setState(STATE.WAITING);
+ 			} else if (detectorId.equals(outsideSensor.getId()) && detected) {
+ 				setState(STATE.BLOCKED);
+ 			}
+ 		case EXITED:
+ 			if (detectorId.equals(insideSensor.getId()) && detected) {
+ 				setState(STATE.EXITING);
+ 			} else if (detectorId.equals(outsideSensor.getId()) && detected) {
+ 				setState(STATE.IDLE);
+ 			}
+ 			// In-progress to do the further development
+ 		default:
+ 			break;
+ 		}
+  	}
+  
+ 	
+ 	
+  }
 
 
-
-	@Override
-	public void carEventDetected(String detectorId, boolean detected) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	
-	
-}
