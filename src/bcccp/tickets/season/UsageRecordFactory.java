11bfca4def0ebf05e78f6a2ad40d3808c725f6a8
mjaved06 <mjaved.jazz@gmail.com>
2017-08-25 16:30:14 +1000

    Updated code in UsageRecordFactory .java

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -2,10 +2,20 @@ package bcccp.tickets.season;
 
 public class UsageRecordFactory implements IUsageRecordFactory {
 
-	@Override
+ArrayList<SeasonTicket> ticketArrayList; 
+	
+
+     public UsageRecordFactory(){ 
+ 		this.ticketArrayList = new ArrayList<SeasonTicket>(); // arraylist been initalized
+   }
+
+
+@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-		// TODO Auto-generated method stub
-		return null;
+		SeasonTicket seasonTicket = new SeasonTicket(ticketId, "22222", startDateTime, 10); 
+ 		ticketArrayList.add(seasonTicket); // object been added to the list. 
+ 		return null; // still returing null 
+
 	}
 
 
