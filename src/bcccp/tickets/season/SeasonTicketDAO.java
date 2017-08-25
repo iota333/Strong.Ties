09a1f39c992c858a80ca3e2886f3f133e8bc0d79
engrhxn <engr.hassan196@gmail.com>
2017-08-25 19:48:15 +1000

    Added Code for SeasonTicketDao.java
    Added classes and code to the file SeasonTicketDao.java. looking forward for review.

--- src/bcccp/tickets/season/SeasonTicketDAO.java
+++ src/bcccp/tickets/season/SeasonTicketDAO.java
@@ -1,12 +1,40 @@
 package bcccp.tickets.season;
+/* Season Class used to create object of season ticket users  
+ */
+package bcccp.tickets.season;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 import bcccp.tickets.season.ISeasonTicket;
 import bcccp.tickets.season.IUsageRecordFactory;
 
 public class SeasonTicketDAO implements ISeasonTicketDAO {
 
 	private IUsageRecordFactory factory;
-
+private IUsageRecord currentUsage = null;
+	
+	private String ticketId;
+	private String carparkId;
+	private long startValidPeriod;
+	private long endValidPeriod;
+	
+	public SeasonTicket (String ticketId, 
+			             String carparkId, 
+			             long startValidPeriod,
+			             long endValidPeriod) {
+		
+		this.ticketId = ticketId;
+		this.carparkId = carparkId;
+		this.startValidPeriod = startValidPeriod;
+		this.endValidPeriod = endValidPeriod;
+	}
 	
 	
 	public SeasonTicketDAO(IUsageRecordFactory factory) {
